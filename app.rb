1df965293414d53619edea9ee2e2bbbdfd2cff31
ronald vilorio <soccer23415@gmail.com>
2018-01-17 18:27:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "#{word1} #{word2} #{word3} #{word4} #{word5}."
   end
   get '/:operation/:number/:number2' do
-    num1 = params[:number1]
+    num1 = params[:number1].to_i
     num2 = params[:number2]
     params[:operation].downcase!
 
