ee52b9974e61dbd359da1cd01fd172373f7fc7e7
ronald vilorio <soccer23415@gmail.com>
2018-01-17 18:36:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     elsif params[:operation] == 'multiply'
       "#{num1 * num2}"
     elsif params[:operation] == 'divide'
-      "#{num1 / num2}" if num2 < num1 || "#{num2 / num1}"
+      "#{num1 / num2}" if num2 > num1 || "#{num2 / num1}"
     else params[:operation].to_i != 0
       "Type add, subtract, multiply, or divide please"
     end
