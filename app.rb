ecc8f011034736f940da24db574217b885298cf7
ronald vilorio <soccer23415@gmail.com>
2018-01-17 18:20:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,8 +23,12 @@ class App < Sinatra::Base
   get '/:operation/:number/:number2' do
     num1 = params[:number1]
     num2 = params[:number2]
+    params[:operation].downcase
     if params[:operation] == 'add'
       "#{num1 + num2}"
+    elsif parms[:operation] == 'subtract'
+
+
 
   end
 
