8516ed4eb0aaf70fc96dada20f6263e0ed2eb04c
ronald vilorio <soccer23415@gmail.com>
2018-01-17 18:23:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,6 +29,8 @@ class App < Sinatra::Base
       "#{num1 + num2}"
     elsif params[:operation] == 'subtract'
       "#{num1 - num2}" || "#{num2 - num1}"
+    elsif params[:operation] == 'multiply'
+
 
 
 
